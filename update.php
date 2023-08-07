e8f06a3e3c70ec9f8c7cb80516bc990ccf78e297
hbh7 <hbh7@hbh7.com>
2023-08-07 15:07:54 -0400

    Improved update script robustness and error reporting

--- update.php
+++ update.php
@@ -11,12 +11,27 @@ if ($cicd_key == null) {
 // This code will update the repo only if it's called via a POST request with the correct auth key
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
    if ($_POST["CICD_KEY"] === $cicd_key) {
-      $output = shell_exec("cd /var/www/start.hbh7.com && git pull 2>&1");
-      echo $output;
-      echo "Update complete.";
+      $output = [];
+      $resultCode = 0;
+      exec("cd /var/www/html/resume && git reset --hard && git pull 2>&1", $output, $resultCode);
+
+      foreach($output as $line) {
+         echo $line . "\n";
+      }
+      if($resultCode != 0) {
+         echo "\nUpdate has failed.";
+         http_response_code(500);
+      } else {
+         echo "\nUpdate complete.";
+         http_response_code(200);
+      }
+
    } else {
       echo "Error: CICD key not valid.";
+      http_response_code(401);
    }
+
 } else {
    echo "Error: GET request not permitted.";
+   http_response_code(405);
 }
